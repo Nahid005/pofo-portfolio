ddb75417e89e89123ce203a16aac4adf9c934d7c
Nahid005 <nahidhassan9258@gmail.com>
2022-08-08 01:44:27 +0600

    layout component design

---
+++ src/component/layout/Layout.jsx
@@ -0,0 +1,81 @@
+
+import classes from './Layout.module.css'
+
+function Layout () {
+    return(
+        <div>
+            <header>
+                <nav>
+                    <div className={classes.brandName}>
+                        <h2> POFO * </h2>
+                    </div>
+                    <div className={classes.navItems}>
+                        <ul>
+                            <li> Home </li>
+                            <li> About </li>
+                            <li> Portfolio </li>
+                            <li> Blog </li>
+                            <li> Contact </li>
+                        </ul>
+                    </div>
+                    <div className={classes.socialIcon}>
+                        <ul>
+                            <li> </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                
+            </main>
+            <footer>
+                <div className={classes.footerTop}>
+                    <p> London based highly creative studio </p>
+                    <div className={classes.brandName}>
+                        <h2> POFO * </h2>
+                    </div>
+                    <div className={classes.socialIcon}>
+
+                    </div>
+                </div>
+                <div className={classes.footerMiddle}>
+                    <div className={classes.footerWidget}>
+                        <div className={classes.footerTitle}>
+                            <h5> ABOUT AGENCY </h5>
+                        </div>
+                        <article>
+                            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the and typesetting industry. </p>
+                        </article>
+                    </div>
+                    <div className={classes.footerWidget}>
+                        <div className={classes.footerTitle}>
+                            <h5> LATEST BLOG POST </h5>
+                        </div>
+                    </div>
+                    <div className={classes.footerWidget}>
+                        <div className={classes.footerTitle}>
+                            <h5> SUBSCRIBE NEWSLETTER </h5>
+                        </div>
+                        <article>
+                            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        </article>
+                        <div className={classes.formGroup}>
+                            <input type="text" name="" id="" placeholder='subscribe newsletter' />
+                        </div>
+                    </div>
+                    <div className={classes.footerWidget}>
+                        <div className={classes.footerTitle}>
+                            <h5> FOLLOW US INSTAGRAM </h5>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.footerBottom}>
+                    <p> POFO - Portfolio Concept Theme </p>
+                    <p> &coppy; 2021 POFO is Proudly Powered by ThemeZaa </p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Layout
